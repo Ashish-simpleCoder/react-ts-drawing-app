e0d637c103e1d2ca6ee1b528b3fa4625fb5869c9
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2024-03-03 11:34:57 +0530

    feat: add drawing feature
    - add use-canvas-drawing hook for drawing features

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -1,57 +1,19 @@
-import { useEffect, useLayoutEffect, useMemo, useState } from 'react'
-import { CANVAS_CLRS, useCanvasAtom, useCanvasAtomValue } from '../atom/canvas.atom'
+import { useEffect, useLayoutEffect } from 'react'
+import { CANVAS_CLRS, useCanvasAtomValue } from '../atom/canvas.atom'
 import { useEventListener } from '../hooks/use-event-listener'
+import useCanvasDrawing from '../hooks/use-canvas-drawing'
 
 export default function Canvas() {
-   const [isDrawing, setIsDrawing] = useState(false)
    const { ref, activeClr, opacity } = useCanvasAtomValue()
+   const { startDrawing, stopDrawing, drawOnCanvas } = useCanvasDrawing()
 
-   useEventListener(
-      () => ref.current,
-      'mousedown',
-      (e) => {
-         setIsDrawing(true)
-      }
-   )
-
-   useEventListener(
-      () => ref.current,
-      'mousemove',
-      (e) => {
-         if (!isDrawing) return
-
-         const ctx = ref.current?.getContext('2d')!
-         const rect = ref.current?.getBoundingClientRect()!
-
-         const x = e.clientX - rect.left
-         const y = e.clientY - rect.top
-         const left = e.pageX
-         const top = e.pageY
-
-         let startPoint = { x, y, left, top }
-
-         ctx.lineJoin = 'round'
-         ctx.lineCap = 'round'
-         ctx.lineWidth = 10
-
-         ctx.beginPath()
-         ctx.moveTo(startPoint.x, startPoint.y)
-         ctx.lineTo(startPoint.x, startPoint.y)
-         ctx.stroke()
-         ctx.beginPath()
-         ctx.globalCompositeOperation = 'source-over'
-         ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI)
-         ctx.fill()
-      }
-   )
-
-   useEventListener(
-      () => ref.current,
-      'mouseup',
-      (e) => {
-         setIsDrawing(false)
-      }
-   )
+   // prettier-ignore
+   useEventListener(() => ref.current, 'mousedown', (e) => {
+      startDrawing()
+      drawOnCanvas(e)
+   })
+   useEventListener(() => ref.current, 'mousemove', drawOnCanvas)
+   useEventListener(() => ref.current, 'mouseup', stopDrawing)
 
    useLayoutEffect(() => {
       ref.current!.width = window.innerWidth
@@ -69,5 +31,5 @@ export default function Canvas() {
       ctx.globalAlpha = opacity
    }, [opacity])
 
-   return <canvas ref={ref} className='bg-[#131315]'></canvas>
+   return <canvas ref={ref} className='bg-[#131315] cursor-crosshair'></canvas>
 }
