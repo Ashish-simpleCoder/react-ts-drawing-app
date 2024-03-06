12c919af6c02c33eb4107b13c28a2603ba904cfb
Ashish Prajapati <62009244+Ashish-simpleCoder@users.noreply.github.com>
2024-03-06 15:54:26 +0000

    feat: persist canvas color, opacity and paths
    - update logic for saving paths in local-storage

--- src/hooks/use-canvas-drawing.tsx
+++ src/hooks/use-canvas-drawing.tsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
 import { flushSync } from 'react-dom'
 
-import { CANVAS_CLRS, useCanvasAtomValue } from '../atom/canvas.atom'
+import { CANVAS_CLRS, CanvasAtom, useCanvasAtomValue } from '../atom/canvas.atom'
 import useLocalStorage from './use-local-storage'
 import useSyncedRef from './use-synced-ref'
 
@@ -13,17 +13,16 @@ export default function useCanvasDrawing() {
    const prevPointRefRef = useRef<{ x: number; y: number; left: number; top: number } | null>(null)
    const currentWorkingPathRef = useRef<Array<[number, number]>>([])
    const moveToPathRef = useRef<Array<[number, number]>>([])
-   const [finalPaths, setFinalPaths] = useLocalStorage<Array<[number, number][]>>('finalPaths', [])
+   const [finalPaths, setFinalPaths] = useLocalStorage<
+      Array<{ path: [number, number][]; color: CanvasAtom['activeClr']; opacity: CanvasAtom['opacity'] }>
+   >('finalPaths', [])
 
    const startDrawing = useCallback(() => setIsDrawing(true), [])
+
    const stopDrawing = useCallback(() => {
       setIsDrawing(false)
       flushSync(() => {
-         // setFinalPaths((paths) =>{
-         //    paths.push(currentWorkingPathRef.current)
-         //    return paths
-         // })
-         finalPaths.push(currentWorkingPathRef.current)
+         finalPaths.push({ color: activeClr, opacity, path: currentWorkingPathRef.current })
          setFinalPaths(finalPaths)
       })
       prevPointRefRef.current = null
@@ -67,64 +66,40 @@ export default function useCanvasDrawing() {
    useEffect(() => {
       const ctx = ref.current?.getContext('2d')!
 
-      // ctx.strokeStyle = CANVAS_CLRS[activeClr]
-      // ctx.fillStyle = CANVAS_CLRS[activeClr]
-      // ctx.globalAlpha = opacity
-
       if (finalPaths.length == 0) return
 
       const drawInitialCanvasPath = () => {
-         let current_path = 0
-         let current_point = 0
+         finalPaths.forEach((activePath) => {
+            let current_point_index = 0
 
-         const draw = () => {
-            // if (current_path >= finalPaths.length) return
-            if (current_path >= finalPaths.length && current_point >= finalPaths[current_path].length) return
+            const draw = () => {
+               const { color, opacity, path } = activePath
+               ctx.strokeStyle = CANVAS_CLRS[color]
+               ctx.fillStyle = CANVAS_CLRS[color]
+               ctx.globalAlpha = opacity
+               ctx.lineJoin = 'round'
+               ctx.lineCap = 'round'
+               ctx.lineWidth = 3
 
-            // increment the path and reset current point
-            // one mouse-down draw will be one path -> it's coordinates are current_point
-            if (current_point >= finalPaths[current_path].length) {
-               // if (current_path < finalPaths.length-1) {
-               current_path++
-               current_point = 0
-               // }
+               if (current_point_index >= path.length) return
+
+               ctx.beginPath()
+               // previous-path
+               ctx.moveTo(path[current_point_index][0], path[current_point_index][1])
+
+               // current-path
+               // prettier-ignore
+               path[current_point_index + 1]?.[0] && ctx.lineTo(path[current_point_index + 1][0], path[current_point_index + 1][1])
+               ctx.stroke()
+
+               current_point_index++
+               draw()
             }
-            if (!finalPaths[current_path]) return
 
-            ctx.lineJoin = 'round'
-            ctx.lineCap = 'round'
-            ctx.lineWidth = 3
-            ctx.strokeStyle = CANVAS_CLRS[activeClr]
-            ctx.fillStyle = CANVAS_CLRS[activeClr]
-            ctx.globalAlpha = opacity
-
-            ctx.beginPath()
-            ctx.moveTo(finalPaths[current_path][current_point][0], finalPaths[current_path][current_point][1])
-            finalPaths[current_path][current_point + 1]?.[0] &&
-               ctx.lineTo(
-                  finalPaths[current_path][current_point + 1][0],
-                  finalPaths[current_path][current_point + 1][1]
-               )
-            ctx.stroke()
-
-            ctx.beginPath()
-            ctx.globalCompositeOperation = 'source-over'
-            ctx.arc(
-               finalPaths[current_path][current_point][0],
-               finalPaths[current_path][current_point][1],
-               1.5,
-               0,
-               2 * Math.PI
-            )
-
-            ctx.fill()
-
-            current_point++
             draw()
-         }
-
-         draw()
+         })
       }
+
       try {
          drawInitialCanvasPath()
       } catch (err) {
@@ -134,3 +109,73 @@ export default function useCanvasDrawing() {
 
    return { isDrawing, setIsDrawing, startDrawing, stopDrawing, drawOnCanvas }
 }
+
+// finalPaths -> Array<[number, number][]>
+// old-structure
+// useEffect(() => {
+//    const ctx = ref.current?.getContext('2d')!
+
+//    // ctx.strokeStyle = CANVAS_CLRS[activeClr]
+//    // ctx.fillStyle = CANVAS_CLRS[activeClr]
+//    // ctx.globalAlpha = opacity
+
+//    if (finalPaths.length == 0) return
+
+//    const drawInitialCanvasPath = () => {
+//       let current_path = 0
+//       let current_point = 0
+
+//       const draw = () => {
+//          // if (current_path >= finalPaths.length) return
+//          if (current_path >= finalPaths.length && current_point >= finalPaths[current_path].length) return
+
+//          // increment the path and reset current point
+//          // one mouse-down draw will be one path -> it's coordinates are current_point
+//          if (current_point >= finalPaths[current_path].length) {
+//             // if (current_path < finalPaths.length-1) {
+//             current_path++
+//             current_point = 0
+//             // }
+//          }
+//          if (!finalPaths[current_path]) return
+
+// ctx.lineJoin = 'round'
+// ctx.lineCap = 'round'
+// ctx.lineWidth = 3
+//          ctx.strokeStyle = CANVAS_CLRS[activeClr]
+//          ctx.fillStyle = CANVAS_CLRS[activeClr]
+//          ctx.globalAlpha = opacity
+
+//          ctx.beginPath()
+//          ctx.moveTo(finalPaths[current_path][current_point][0], finalPaths[current_path][current_point][1])
+//          finalPaths[current_path][current_point + 1]?.[0] &&
+//             ctx.lineTo(
+//                finalPaths[current_path][current_point + 1][0],
+//                finalPaths[current_path][current_point + 1][1]
+//             )
+//          ctx.stroke()
+
+//          ctx.beginPath()
+//          ctx.globalCompositeOperation = 'source-over'
+//          ctx.arc(
+//             finalPaths[current_path][current_point][0],
+//             finalPaths[current_path][current_point][1],
+//             1.5,
+//             0,
+//             2 * Math.PI
+//          )
+
+//          ctx.fill()
+
+//          current_point++
+//          draw()
+//       }
+
+//       draw()
+//    }
+//    try {
+//       drawInitialCanvasPath()
+//    } catch (err) {
+//       console.log(err)
+//    }
+// }, [])
